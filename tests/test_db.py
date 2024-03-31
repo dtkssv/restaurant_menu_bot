3882e4e915d88963a461039a8d98ea0fa67b04b8
Valentin Belyakov <devil1546@yandex.ru>
2024-03-31 20:37:25 +0300

    !!!

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,5 +1,5 @@
 from restaurant_menu.models.database import *
 
-c1 = Client(chat_id = 12345567,
-            name = "Oleg")
+c1 = Client(chat_id=12345567,
+            name="Oleg")
 SessionLocal.add(c1)
